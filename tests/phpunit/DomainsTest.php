518d26f4a8d65074336675cdf94c28e7635779be
Kunal Mehta <legoktm@member.fsf.org>
2019-12-13 23:03:21 -0800

    Add sanity check test for domains.php
    The test verifies:
    * domains.php returns an array
    * The array has at least 50,000 items (right now it has ~78,000)
    * Each domain in the array passes a regex, and the value is either 1 or 0
    
    Bug: T240743
    Change-Id: Ic4e2c2e36ddcb0f775f4ac22d1418c814db5b389

---
+++ tests/phpunit/DomainsTest.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Copyright (C) 2019 Kunal Mehta <legoktm@member.fsf.org>
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ */
+
+namespace MediaWiki\SecureLinkFixer\Test;
+
+use MediaWikiTestCase;
+
+/**
+ * Integration test and sanity check for domains.php
+ * @coversNothing
+ */
+class DomainsTest extends MediaWikiTestCase {
+
+	public function testDomains() {
+		$domains = require __DIR__ . '/../../domains.php';
+		$this->assertIsArray( $domains );
+		// Some arbitrary number as a sanity check
+		$this->assertGreaterThan( 50000, count( $domains ) );
+		foreach ( $domains as $domain => $subdomain ) {
+			$this->assertRegExp( '/^[A-z0-9\-\.]*$/', $domain );
+			$this->assertTrue( $subdomain === 0 || $subdomain === 1 );
+		}
+	}
+}
