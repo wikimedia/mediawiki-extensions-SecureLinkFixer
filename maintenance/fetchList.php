fee7ab5a42f98e2dbeb7baab9a78d9ed5f974559
libraryupgrader <tools.libraryupgrader@tools.wmflabs.org>
2021-05-13 09:45:29 +0000

    build: Updating dependencies
    composer:
    * mediawiki/mediawiki-codesniffer: 35.0.0 → 36.0.0
    * php-parallel-lint/php-parallel-lint: 1.2.0 → 1.3.0
    
    npm:
    * grunt: 1.3.0 → 1.4.0
    * lodash: 4.17.19 → 4.17.21
      * https://npmjs.com/advisories/1673 (CVE-2021-23337)
    
    Change-Id: I65eb5506046363d6e8afcaf3166a5f153865121a

--- maintenance/fetchList.php
+++ maintenance/fetchList.php
@@ -33,7 +33,7 @@ require_once __DIR__ . '/../includes/ListFetcher.php';
  * can be run without needing all of MediaWiki to be installed.
  */
 function main() {
-	$lf = new ListFetcher( function ( $text ) {
+	$lf = new ListFetcher( static function ( $text ) {
 		echo $text;
 	} );
 	[ $rev, $date ] = $lf->getLatestInfo();
